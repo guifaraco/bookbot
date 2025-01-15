d71d74a974f24cae56b8352301e0682ff118936c
Guilherme Vieira <guirfv29@gmail.com>
2025-01-14 21:31:48 -0300

    add count_words function that counts the number of words in the string

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+def count_words(file_contents):
+    return len(file_contents.split())
+
 def main():
     with open("books/frankenstein.txt") as f:
         file_contents = f.read()
-        print(file_contents)
+        print(count_words(file_contents))
 
 main()
