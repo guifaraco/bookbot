7faddb4b0e2dee141ec962b49b5c348a1c0b3e51
Guilherme Vieira <guirfv29@gmail.com>
2025-01-14 22:44:10 -0300

    add chars_dict_to_sorted_list and sort_on function so I can print how many times the char is found sorted by the number

--- main.py
+++ main.py
@@ -2,19 +2,37 @@ def main():
         path = "books/frankenstein.txt"
         file_contents = get_book_text(path)
         word_count = count_words(file_contents)
-        character_count = count_characters(file_contents)
-        print(f"The number of words in the book is {word_count}")
-        print(f"The count of characters is {character_count}")
+        chars_num_dict = get_chars_dict(file_contents)
+        sorted_list = chars_dict_to_sorted_list(chars_num_dict)
 
+        print(f"--- {path} ---")
+        print(f"{word_count} words found in the document")
 
-def count_characters(text):
-    character_count = {}
+        for dictionary in sorted_list:
+            if not dictionary["character"].isalpha():
+                continue
+            print(f"The '{dictionary["character"]}' character was found {dictionary["number"]} times")
+
+        print("--- END ---")
+
+def sort_on(dictionary):
+    return dictionary["number"]
+
+def chars_dict_to_sorted_list(chars_num_dict):
+    sorted_list = []
+    for character in chars_num_dict:
+        sorted_list.append({"character": character, "number": chars_num_dict[character]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
+
+def get_chars_dict(text):
+    chars_num_dict = {}
     for character in text.lower():
-        if character in character_count:
-            character_count[character] += 1
+        if character in chars_num_dict:
+            chars_num_dict[character] += 1
         else:
-            character_count[character] = 1
-    return character_count
+            chars_num_dict[character] = 1
+    return chars_num_dict
 
 def count_words(text):
     return len(text.split())
